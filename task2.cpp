c491d7d27749b3a797fb1e0689b9e56255dbee70
andrey <naumov.a.n@icloud.com>
2024-02-04 02:26:17 +0700

    update task2 метод отрисовки, заполнения, ход(преработать)

--- task2.cpp
+++ task2.cpp
@@ -8,27 +8,65 @@ using namespace std;
 /*Крестики-нолики*/
 
 const int ROW = 3;
-const int COLUMN = 8;
+const int COLUMN = 3;
 
-void printConsole(char arr[ROW][COLUMN], string text) {
-	cout << text << endl;
-	for (int i = 0; i < ROW; i++) {
-		for (int j = 0; j < COLUMN; j++) {
-			cout << arr[i][j] << " ";
+char ticTacToe[ROW][COLUMN]{};
+
+void drawBoard(char ticTacToe[ROW][COLUMN]) {
+	for (int i = 0; i < ROW; ++i) {
+		for (int j = 0; j < COLUMN; ++j) {
+			cout << ticTacToe[i][j];
+			if (j < COLUMN - 1) {
+				cout << "|";
+			}
 		}
-		cout << endl;
+		cout << std::endl;
+		if (i < ROW -1) {
+			for (int k = 0; k < COLUMN; ++k) {
+				std::cout << "--" ;
+			}
+			cout << endl;
+		}
 	}
-	cout << endl;
+}
+
+void initTicTacToe() {
+	for (int row = 0; row < ROW; ++row) {
+		for (int column = 0; column < COLUMN; ++column) {
+			ticTacToe[row][column] = ' ';
+		}
+	}
+	drawBoard(ticTacToe);
+}
+
+void checkingCapacity() {
+
+}
+
+void winning() {
+
+}
+
+void gameUser() {
+	while (true) {
+		int x, y;
+		cout << "Ход первого игрока";
+		cin >> x >> y;
+		checkingCapacity();
+		ticTacToe[x][y] = 'X';
+		drawBoard(ticTacToe);
+		winning();
+
+		cout << "Ход второго игрока";
+		cin >> x >> y;
+		checkingCapacity();
+		ticTacToe[x][y] = 'O';
+		drawBoard(ticTacToe);
+		winning();
+	}
 }
 
 void task2() {
-	char ticTacToe[ROW][COLUMN]	{};
-
-	for (int row = 0; row < ROW; ++row) {
-		for (int column = 0; column < COLUMN; ++column) {
-			ticTacToe[row][column] = 'Z';
-		}
-	}
-
-	printConsole(ticTacToe, "Поле");
+	initTicTacToe();
+	gameUser();
 }
